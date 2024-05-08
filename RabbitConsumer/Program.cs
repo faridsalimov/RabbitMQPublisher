db3b921aa9fa4a29b64b9911d60766b6935e5b2a
Selim_eh58 <Selim_eh58@STHQ0118-13>
2024-05-08 11:25:54 +0400

    Add project files.

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://jkspswgo:DUo0Ur4zC38A5yR7V_sBCeEtgZDaqpvy@roedeer.rmq.cloudamqp.com/jkspswgo");
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("step-queue", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("step-queue", true, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine($"Received Message: {message}");
+        }
+    }
+}
---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitPublisher
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://jkspswgo:DUo0Ur4zC38A5yR7V_sBCeEtgZDaqpvy@roedeer.rmq.cloudamqp.com/jkspswgo");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            while (true)
+            {
+                channel.QueueDeclare("step-queue", true, false, false);
+
+                var message = "[" + DateTime.Now.ToLongTimeString() + "] Test Message.";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "step-queue", null, body);
+
+                Console.WriteLine("Sent successfully.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
